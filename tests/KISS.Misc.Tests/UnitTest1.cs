738cbcee7a48726811ca89e1278471879b2ea1b5
ltuan <lttuan846@gmail.com>
2024-05-18 01:30:53 +0700

    init structures

---
+++ tests/KISS.Misc.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace KISS.Misc.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/KISS.QueryBuilder.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace KISS.QueryBuilder.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
