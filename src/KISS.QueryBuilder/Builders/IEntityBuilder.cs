2dca5c2de279125d428f328a08df02d1495310f7
ltuan <lttuan846@gmail.com>
2025-03-11 02:07:41 +0700

    Remove old project

--- src/KISS.QueryBuilder/Builders/IEntityBuilder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace KISS.QueryBuilder.Builders;
-
-/// <summary>
-///     Serves as a base interface for building and configuring models that map to database tables.
-/// </summary>
-public interface IEntityBuilder;
--- src/KISS.QueryBuilder/Builders/OrderByBuilders/IOrderByBuilder.cs
+++
@@ -1,7 +0,0 @@
-namespace KISS.QueryBuilder.Builders.OrderByBuilders;
-
-/// <summary>
-///     An interface for building SQL ORDER BY clauses.
-/// </summary>
-/// <typeparam name="TRecordset">The type representing the database record set.</typeparam>
-public interface IOrderByBuilder<TRecordset> : ILimitBuilder<TRecordset>;
