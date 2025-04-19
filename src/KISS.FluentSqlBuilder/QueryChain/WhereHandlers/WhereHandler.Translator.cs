eac528d97c46c04232bb88f6853d6656bde15ebc
ltuan <lttuan846@gmail.com>
2025-04-20 02:08:39 +0700

    update builder

--- src/KISS.FluentSqlBuilder/QueryChain/WhereHandlers/WhereHandler.Translator.cs
+++ src/KISS.FluentSqlBuilder/QueryChain/WhereHandlers/WhereHandler.Translator.cs
@@ -43,8 +43,7 @@ public sealed partial record WhereHandler
                     break;
                 }
 
-            // Accessing a field/property of a constant object
-            case ConstantExpression constantExpression:
+            default:
                 {
                     var (evaluated, value) = Composite.GetValue(memberExpression);
                     if (evaluated)
@@ -53,27 +52,9 @@ public sealed partial record WhereHandler
                     }
                     else
                     {
-                        Translate(constantExpression);
+                        Translate(memberExpression.Expression);
                     }
 
-                    break;
-                }
-
-            // Accessing an object creation, method invocation, or method call (e.g., new Object().Property, obj.Method().Property)
-            case NewExpression:
-            case InvocationExpression:
-            case MethodCallExpression:
-                {
-                    // Accessing the property or field (memberExpression.Member.Name) on the object or result of the method (memberExpression.Expression).
-                    var member = Expression.Property(memberExpression.Expression, memberExpression.Member.Name);
-                    var value = Expression.Lambda(member).Compile().DynamicInvoke();
-                    AppendFormat($"{value}");
-                    break;
-                }
-
-            default:
-                {
-                    Translate(memberExpression.Expression);
                     break;
                 }
         }
@@ -126,82 +107,98 @@ public sealed partial record WhereHandler
     }
 
     /// <summary>
+    ///     Translates a unary expression into SQL for aggregate operations.
+    ///     Handles operations like negation and type conversion.
+    /// </summary>
+    /// <param name="unaryExpression">The unary expression to translate.</param>
+    protected override void Translate(UnaryExpression unaryExpression)
+    {
+        if (unaryExpression is { Operand: { } expression })
+        {
+            Translate(expression);
+        }
+    }
+
+    /// <summary>
     ///     Translates a method call expression into SQL.
     ///     Handles special SQL functions like InRange, AnyIn, and NotIn.
     /// </summary>
     /// <param name="methodCallExpression">The method call expression to translate.</param>
     protected override void Translate(MethodCallExpression methodCallExpression)
     {
-        if (methodCallExpression.Method.DeclaringType == typeof(SqlFunctions))
+        switch (methodCallExpression)
         {
-            const string inRange = nameof(SqlFunctions.InRange);
-            const string anyIn = nameof(SqlFunctions.AnyIn);
-            const string notIn = nameof(SqlFunctions.NotIn);
+            case { Method: { } t } when t.DeclaringType == typeof(SqlFunctions):
+                const string inRange = nameof(SqlFunctions.InRange);
+                const string anyIn = nameof(SqlFunctions.AnyIn);
+                const string notIn = nameof(SqlFunctions.NotIn);
 
-            var mi = typeof(SqlFunctions)
-                .GetMethods(BindingFlags.Static | BindingFlags.Public)
-                .Single(mt => mt.IsGenericMethod && mt.Name == methodCallExpression.Method.Name);
-
-            switch (mi.Name)
-            {
-                case inRange:
-                    {
-                        const string betweenOp = " BETWEEN ";
-                        const string andOp = " AND ";
-
-                        if (methodCallExpression.Arguments is
-                            [var fieldAsExpression, var beginAsExpression, var endAsExpression])
+                switch (t.Name)
+                {
+                    case inRange:
                         {
-                            OpenParentheses();
-                            Translate(fieldAsExpression);
+                            const string betweenOp = " BETWEEN ";
+                            const string andOp = " AND ";
 
-                            Append(betweenOp);
-                            Translate(beginAsExpression);
+                            if (methodCallExpression.Arguments is
+                                [var fieldAsExpression, var beginAsExpression, var endAsExpression])
+                            {
+                                OpenParentheses();
+                                Translate(fieldAsExpression);
 
-                            Append(andOp);
-                            Translate(endAsExpression);
-                            CloseParentheses();
+                                Append(betweenOp);
+                                Translate(beginAsExpression);
+
+                                Append(andOp);
+                                Translate(endAsExpression);
+                                CloseParentheses();
+                            }
+
+                            break;
                         }
 
-                        break;
-                    }
-
-                case anyIn:
-                    {
-                        const string inOp = " IN ";
-
-                        if (methodCallExpression.Arguments is
-                            [var fieldAsExpression, var valuesAsExpression])
+                    case anyIn:
                         {
-                            OpenParentheses();
-                            Translate(fieldAsExpression);
+                            const string inOp = " IN ";
 
-                            Append(inOp);
-                            Translate(valuesAsExpression);
-                            CloseParentheses();
+                            if (methodCallExpression.Arguments is
+                                [var fieldAsExpression, var valuesAsExpression])
+                            {
+                                OpenParentheses();
+                                Translate(fieldAsExpression);
+
+                                Append(inOp);
+                                Translate(valuesAsExpression);
+                                CloseParentheses();
+                            }
+
+                            break;
                         }
 
-                        break;
-                    }
-
-                case notIn:
-                    {
-                        const string notInOp = " NOT IN ";
-
-                        if (methodCallExpression.Arguments is
-                            [var fieldAsExpression, var valuesAsExpression])
+                    case notIn:
                         {
-                            OpenParentheses();
-                            Translate(fieldAsExpression);
+                            const string notInOp = " NOT IN ";
 
-                            Append(notInOp);
-                            Translate(valuesAsExpression);
-                            CloseParentheses();
+                            if (methodCallExpression.Arguments is
+                                [var fieldAsExpression, var valuesAsExpression])
+                            {
+                                OpenParentheses();
+                                Translate(fieldAsExpression);
+
+                                Append(notInOp);
+                                Translate(valuesAsExpression);
+                                CloseParentheses();
+                            }
+
+                            break;
                         }
+                }
 
-                        break;
-                    }
-            }
+                break;
+
+            default:
+                var (evaluated, value) = Composite.GetValue(methodCallExpression);
+                break;
         }
     }
 }
